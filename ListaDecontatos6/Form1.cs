e0759e12bbe5869471c22f994391fbd0b2ae28d1
Kauê Lovisi de Paula <kauelovisi@gmail.com>
2024-04-25 15:24:21 -0300

    criado metodo Tostring()
    metodo criado

--- ListaDecontatos6/Form1.cs
+++ ListaDecontatos6/Form1.cs
@@ -8,13 +8,79 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ListaDecontatos6
+namespace ListaDeContatosT6
 {
-    public partial class Form1 : Form
+    internal class Contato
     {
-        public Form1()
+        private string nome;
+        private string sobrenome;
+        private string telefone;
+
+        public string Nome
         {
-            InitializeComponent();
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
         }
+        public string Sobrenome
+        {
+            get
+            {
+                return sobrenome;
+            }
+            set
+            {
+                sobrenome = value;
+            }
+        }
+        public string Telefone
+        {
+            get
+            {
+                return telefone;
+            }
+            set
+            {
+                if (value.Length == 13)
+                {
+                    telefone = value;
+                }
+                else
+                {
+                    telefone = "(00) 00000-0000";
+                }
+            }
+        }
+        public Contato()
+        {
+            Nome = "claudio";
+            sobrenome = "genesio 2";
+            telefone = "(11) 96613-0331";      
+        }
+        public Contato(string nome, string sobrenome, string telefone)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Telefone = telefone;
+
+        }
+        public override string.ToString()
+        {
+            string saida = string.Empty;
+            saida += string.Format("{0} {1} ", Nome, Sobrenome);
+            saida += string.Format("{0} {1}-{2} ", 
+                Telefone.Substring(0, 2),
+                Telefone.Substring(3, 5),
+                Telefone).Substring(7, 4));
+            return saida;
+        }
+        
     }
 }
+
